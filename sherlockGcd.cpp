b4d6ae1b1ddcab542b1c088a61bb485fcc807989
bokariasiddhartha@gmail.com <bokariasiddhartha@gmail.com>
2014-08-13 20:59:54 +0530

    sherlockGCD modified

--- sherlockGcd.cpp
+++ sherlockGcd.cpp
@@ -1,40 +1,38 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-int checkprime(int n)
-    {
-    int i=sqrt(n);
-    for(int j=2;j<i;j++)
-        {
-        if(n%j==0)
-        return -1;
-    }
-return 1;
+int gcd(int a, int b)
+{
+int t;
+  while(a)
+{
+    t=a;
+    a=b%a;
+    b=t;  
+  }  
+return b;
 }
 int main()
 {
-    int t,flag;
+    int t,ans,n;
     cin>>t;
     while(t--)
     {
-        flag=-1;
-        int n;
-        cin>>n;
-        int a[n];
-        for(int i=0;i<n;i++)
-        cin>>a[i];
-        for(int i=0;i<n;i++)
-        {
-            
-           flag= checkprime(a[i]);
-            if(flag==-1)
-            {
-                cout<<"NO\n";
-                break;
-            }
-        }
-    if(flag==1)
-    cout<<"YES\n";     
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    
+    ans=a[0];
+    for(int i=1;i<n;i++)
+    {
+        ans = gcd(ans,a[i]);
     }
-    return 0;   
+    if(ans==1)
+    cout<<"YES\n";
+    else
+    cout<<"NO\n";
+    }
+    
+   return 0;   
 }
